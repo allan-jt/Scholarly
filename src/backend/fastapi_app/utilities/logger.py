9df73f931fd3535a7e43c6061abec4708156da19
timothycao <tcao.pro@gmail.com>
2024-12-16 14:26:13 -0500

    Removed start message in loggers

--- src/backend/fastapi_app/utilities/logger.py
+++ src/backend/fastapi_app/utilities/logger.py
@@ -4,13 +4,13 @@ from contextlib import contextmanager, asynccontextmanager
 @contextmanager
 def log(message: str):
     start_time = time.time()
-    print(f'{message}...')
+    # print(f'{message}...')
     yield
     print(f'{message} completed in {time.time() - start_time:.2f} seconds.')
 
 @asynccontextmanager
 async def log_async(message: str):
     start_time = time.time()
-    print(f'{message}...')
+    # print(f'{message}...')
     yield
     print(f'{message} completed in {time.time() - start_time:.2f} seconds.')
