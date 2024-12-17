d1d4988bfd779c1e9f4ec0e1f3df811ad780f3ab
Ahhyun Moon <am12180@nyu.edu>
2024-12-16 19:57:05 -0500

    Revert "Added wave in dark mode"
    This reverts commit 19312626d72c3f3c53779b1348b0ea9a8c606dbd.

--- src/frontend/react_app/src/search/Wave.tsx
+++ src/frontend/react_app/src/search/Wave.tsx
@@ -1,37 +1,14 @@
 import GitHubIcon from "@mui/icons-material/GitHub";
 import "../App.css";
-import lightWave from "../assets/lightWave.png";
-import darkWave from "../assets/darkWave.png";
-import { CSSProperties } from "react";
 
 export default function Wave() {
-    const savedTheme =
-        typeof window !== "undefined"
-            ? localStorage.getItem("theme") || "light"
-            : "light";
-    const waveImg = savedTheme === "light" ? lightWave : darkWave;
-    const waveColor = savedTheme === "light" ? "#36beff" : "#016391";
-
-    const waveStyles: CSSProperties = {
-        backgroundRepeat: "repeat-x",
-        backgroundSize: "1000px 100px",
-    };
-
-    const waves = ["wave1", "wave2", "wave3", "wave4"];
     return (
-        <footer style={{ background: waveColor }}>
-            <div>
-                {waves.map((item, index) => (
-                    <div
-                        key={index}
-                        className="wave"
-                        id={item}
-                        style={{
-                            backgroundImage: `url(${waveImg})`,
-                            ...waveStyles,
-                        }}
-                    ></div>
-                ))}
+        <footer>
+            <div className="waves">
+                <div className="wave" id="wave1"></div>
+                <div className="wave" id="wave2"></div>
+                <div className="wave" id="wave3"></div>
+                <div className="wave" id="wave4"></div>
             </div>
             <ul className="social-icons">
                 <li>
